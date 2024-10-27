b065a5ac560c38bf74cbdae90bd746c57955e2ff
renzoSalosagcol <renzosalosagcol@csu.fullerton.edu>
2024-10-27 16:17:39 -0700

    pnpm fix

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 -- Note that the SQLite3 dump isn't quite right for PostgreSQL.
 -- 1. "number" needs to be "integer" or "float".
 -- 2. "date" needs to be "date".
--- 3. id/rowid needs to be "id serial primary key"
+-- 3. id/rowid needs to be "id serial primary key"s
 -- 4. quoted strings in the CREATE INDEX need the quotes removed.
 
 CREATE TABLE containers (containerNumber varchar, nameOfShip varchar, containerSize float, dateContainerShipped varchar);
